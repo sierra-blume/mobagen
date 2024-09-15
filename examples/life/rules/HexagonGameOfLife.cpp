98c4667c1e1d691f1ea3539b6c300ae4b4f66a40
Sierra Blume <sierra.blume@mymail.champlain.edu>
2024-09-15 16:29:10 -0400

    Completed John Conway rule set

--- examples/life/rules/HexagonGameOfLife.cpp
+++ examples/life/rules/HexagonGameOfLife.cpp
@@ -5,3 +5,11 @@
 #include "HexagonGameOfLife.h"
 void HexagonGameOfLife::Step(World& world) {}
 int HexagonGameOfLife::CountNeighbors(World& world, Point2D point) { return 0; }
+
+/*
+bool topLeft(World &world, Point2D point) {
+  if (point.y % 2 == 0)
+    return world.Get({point.x-1, point.y-1});
+  else
+    return world.Get({point.x, point.y-1});
+}*/
--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -3,9 +3,35 @@
 // Reference: https://playgameoflife.com/info
 void JohnConway::Step(World& world) {
   // todo: implement
+
+  for(int y = 0; y < world.SideSize(); y++) {
+    for (int x = 0; x < world.SideSize(); x++) {
+      Point2D p = Point2D(x, y);
+      if (world.Get(p)) {
+        if (CountNeighbors(world, p) < 2 || CountNeighbors(world, p) > 3)
+          world.SetNext(p, false);
+        else
+          world.SetNext(p, true);
+      }
+      else {
+        if (CountNeighbors(world, p) == 3)
+          world.SetNext(p, true);
+      }
+    }
+  }
+  world.SwapBuffers();
 }
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
   // todo: implement
-  return 0;
+  int neighbors = 0;
+
+  for (int y = -1; y <= 1; y++) {
+    for (int x = -1; x <= 1; x++) {
+      Point2D p = Point2D((point.x + x) % world.SideSize(), (point.y + y) % world.SideSize());
+      if(p != point && world.Get(p))
+        neighbors++;
+    }
+  }
+  return neighbors;
 }
