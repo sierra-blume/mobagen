38a3152b1f9bd454863c2543f6eb9d99d66bd740
Sierra Blume <sierra.blume@mymail.champlain.edu>
2024-10-23 23:39:42 -0400

    Added more to the generate path function. Will be polishing to be A* next

--- examples/catchthecat/Agent.cpp
+++ examples/catchthecat/Agent.cpp
@@ -4,6 +4,20 @@
 #include <queue>
 #include "World.h"
 using namespace std;
+
+std::vector<Point2D> getVisitableNeighbors(World* w, Point2D current, unordered_set<Point2D> frontierSet, unordered_map<Point2D, bool> visited) {
+  std::vector<Point2D> allNeighbors = w->neighbors(current);
+  std::vector<Point2D> visitableNeighbors;
+  for (auto neighbor : allNeighbors) {
+    if (w->catCanMoveToPosition(neighbor) && !frontierSet.contains(neighbor) && !visited.contains(neighbor)) {
+      visitableNeighbors.push_back(neighbor);
+    }
+  }
+  return visitableNeighbors;
+}
+
+
+
 std::vector<Point2D> Agent::generatePath(World* w) {
   unordered_map<Point2D, Point2D> cameFrom;  // to build the flowfield and build the path
   queue<Point2D> frontier;                   // to store next ones to visit
@@ -21,13 +35,22 @@ std::vector<Point2D> Agent::generatePath(World* w) {
     Point2D current = frontier.front();
     // remove the current from frontierset
     frontier.pop();
+    frontierSet.erase(current);
     // mark current as visited
     visited[current] = true;
     // getVisitableNeightbors(world, current) returns a vector of neighbors that are not visited, not cat, not block, not in the queue
+    std::vector<Point2D> visitableNeighbors = getVisitableNeighbors(w, current, frontierSet, visited);
+
     // iterate over the neighs:
     // for every neighbor set the cameFrom
     // enqueue the neighbors to frontier and frontierset
+    for (auto neighbor : visitableNeighbors) {
+      cameFrom.insert({neighbor, current});
+      frontier.push(neighbor);
+      frontierSet.insert(neighbor);
+    }
     // do this up to find a visitable border and break the loop
+    if (w->catWinsOnSpace(current)) {break;}
   }
 
   // if the border is not infinity, build the path from border to the cat using the camefrom map
--- examples/catchthecat/Agent.h
+++ examples/catchthecat/Agent.h
@@ -2,6 +2,8 @@
 #define AGENT_H
 #include "math/Point2D.h"
 #include <vector>
+#include <unordered_set>
+#include <unordered_map>
 
 class World;
 
@@ -20,6 +22,8 @@ public:
   explicit Agent() = default;
 
   virtual Point2D Move(World*) = 0;
+
+  std::vector<Point2D> getVisitableNeighbors(World* w, Point2D current, std::unordered_set<Point2D> frontierSet, std::unordered_map<Point2D, bool> visited);
 
   std::vector<Point2D> generatePath(World* w);
 };
